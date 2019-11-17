b6b9ff1afcf60d95977d4e35394120504b82da41
Kamalyesh Kannadkar <kkamalyesh@gmail.com>
2019-11-17 17:11:23 +0530

    changed index file

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 let test = require('./tests');
 
 
-// TODO: see if JOI can be used for validations here too
-if(test.numbers()){
-    console.log('test of numbers is successful');
-}else{
-    console.log('test of numbers has failed');
-};
-if(test.operations()){
-    console.log('test of operations is successful');
-}else{
-    console.log('test of operations has failed');
+testNums = () => {
+    // TODO: see if JOI can be used for validations here too
+    if (test.numbers()) {
+        console.log('test of numbers is successful');
+        return 'test of numbers is successful';
+    } else {
+        console.log('test of numbers has failed');
+        return 'test of numbers has failed';
+    };
 }
 
-console.log('CHANGE THE FILES TO SUITE NEW FORMAT');
-console.log('THIS NEW FORMAT WILL MAKE VERSION 1.0.1');
+testOps = () => {
 
+    if (test.operations()) {
+        console.log('test of operations is successful');
+        return 'test of operations is successful';
+    } else {
+        console.log('test of operations has failed');
+        return 'test of operations has failed';
+    }
+}
+
+todo = () => {
+    console.log('CHANGE THE FILES TO SUITE NEW FORMAT');
+    console.log('THIS NEW FORMAT WILL MAKE VERSION 1.0.1');
+}
+
+todo();
+testNums();
+testOps();
