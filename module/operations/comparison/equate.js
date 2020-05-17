a3dffc47b21ba5c631e5b3fd9aa0e164092117db
k-kamalyesh <Raven@Magic7>
2020-05-17 21:25:17 +0530

    checking if numbers have same base or not

--- module/operations/comparison/equate.js
+++ module/operations/comparison/equate.js
@@ -3,18 +3,20 @@ module.exports = {
         // returns true if two numbers are equal
         // does not considers sign of numbers
 
-        let base;
-        if (num1._base.value == num2._base.value) base = num1._base;
-        else throw new Error('invald operation! base mismatch!');
+        let base1, base2;
 
         if (typeof num1 != 'string') {
             num1._trimZero();
+            base1 = num1._base.value;
             num1 = num1._value;
         }
         if (typeof num2 != 'string') {
             num2._trimZero();
+            base2 = num2._base.value;
             num2 = num2._value;
         }
+
+        if (base1 !== base2) throw new Error('invald operation! base mismatch!');
 
         // check for signs of both
         if ((num1.startsWith('-') && !num2.startsWith('-')) || (!num1.startsWith('-') && num2.startsWith('-'))) {
@@ -32,18 +34,20 @@ module.exports = {
         // returns true if numbers are equal
         // considers signs in comparison
 
-        let base;
-        if (num1._base.value == num2._base.value) base = num1._base;
-        else throw new Error('invald operation! base mismatch!');
+        let base1, base2;
 
         if (typeof num1 != 'string') {
             num1._trimZero();
+            base1 = num1._base.value;
             num1 = num1._value;
         }
         if (typeof num2 != 'string') {
             num2._trimZero();
+            base2 = num2._base.value;
             num2 = num2._value;
         }
+
+        if (base1 !== base2) throw new Error('invald operation! base mismatch!');
 
         // test equivalence
         if (num1 !== num2) {
